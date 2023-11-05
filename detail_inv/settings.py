dbea93e2ed8ef710efc1f71b8f6478ba8024dd50
Gajanan kale <111948259+gajanankumar@users.noreply.github.com>
2023-11-05 22:30:47 +0530

    Update settings.py

--- detail_inv/settings.py
+++ detail_inv/settings.py
@@ -79,13 +79,14 @@ WSGI_APPLICATION = 'detail_inv.wsgi.application'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
 DATABASES = {
+  
     'default': {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'inv',
         'PORT' : 3306,
         'USER' : 'root',
         'PASSWORD' : 'kale',
-        'HOST':'localhost'
+        'HOST':'mysql'
         
     },
     'test': {
